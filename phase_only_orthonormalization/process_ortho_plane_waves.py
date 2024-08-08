d696d9b93bc190ec34cab5a787e5a918d335a778
Daniel Cox <danielcox16@hotmail.com>
2024-08-08 16:54:40 +0200

    Reorder things. Plot untransformed grid.

--- phase_only_orthonormalization/process_ortho_plane_waves.py
+++ phase_only_orthonormalization/process_ortho_plane_waves.py
@@ -18,6 +18,10 @@ do_plot_transformed_gridmap = False
 # filepath = 'C:/LocalData/ortho-plane-waves.hdf5'
 filepath = '/home/dani/LocalData/ortho-plane-waves-1.hdf5'
 
+# Transformed grids
+arc_linewidth = 1.6
+grid_linewidth = 0.7
+
 # Jacobian plot
 cmap = 'magma'
 
@@ -72,6 +76,8 @@ if do_plot_bases:
         plot_field(new_modes[:, :, m], scale=scale)
         plt.xticks([])
         plt.yticks([])
+        plt.xlim((domain['x_min'], domain['x_max']))
+        plt.ylim((domain['y_min'], domain['y_max']))
 
     # Complex colorwheel
     center_spi = int(n_cols_basis + 1 + np.floor(n_rows/2) * n_cols_total)
@@ -85,31 +91,17 @@ if do_plot_bases:
 
 # Plot transformed grid and Jacobian
 if do_plot_transform_jacobian:
-    # Normalization factor
-    area = (domain['x_max'] - domain['x_min']) * (domain['y_max'] - domain['y_min'])
-    num_samples = np.prod(domain['yxshape'])
-    norm_factor = num_samples / area
-
-    # Amplitude truncated Gaussian
-    x, y = get_coords(domain)
-    amp_unnorm_sq = trunc_gaussian(x, y, **amplitude_kwargs) ** 2       # Non-normalized amplitude squared
-    amp_sq = amp_unnorm_sq * norm_factor / amp_unnorm_sq.sum()          # Normalized amplitude
-
-    # Amplitude rectangle
-    wx, wy, jacobian = coord_transform(x=x, y=y, a=a, b=b, p_tuple=p_tuple, q_tuple=q_tuple, compute_jacobian=True)
-    amp_rect_sq_transformed = amplitude_rectangle(x=wx, y=wy, domain=domain) ** 2   # Transformed amplitude rectangle
-    amp_sq_approx = norm_factor * (amp_rect_sq_transformed * jacobian.abs())        # Approx. to original amplitude²
-
     # Prepare plot
     fig = plt.figure(figsize=(16, 8))
     plt.subplots_adjust(left=0.01, right=0.99, top=0.96, bottom=0.01)
 
-
     # === Plot transformed grid === #
     gridsize_x = 1 + 8 * (domain['x_max'] - domain['x_min'])
     gridsize_y = 1 + 8 * (domain['y_max'] - domain['y_min'])
-    x_grid = torch.linspace(domain['x_min'], domain['x_max'], gridsize_x).view(1, -1, 1, 1, 1)  # Normalized x
-    y_grid = torch.linspace(domain['y_min'], domain['y_max'], gridsize_y).view(-1, 1, 1, 1, 1)  # Normalized y
+    x_grid_lin = torch.linspace(domain['x_min'], domain['x_max'], gridsize_x)
+    x_grid = x_grid_lin.view(1, -1, 1, 1, 1).broadcast_to((gridsize_y, gridsize_x, 1, 1, 1))
+    y_grid_lin = torch.linspace(domain['y_min'], domain['y_max'], gridsize_y)
+    y_grid = y_grid_lin.view(-1, 1, 1, 1, 1).broadcast_to((gridsize_y, gridsize_x, 1, 1, 1))
     r_mask = x_grid * x_grid + y_grid * y_grid > 1.01
 
     wx_grid, wy_grid = coord_transform(x_grid, y_grid, a, b, p_tuple, q_tuple)
@@ -125,16 +117,47 @@ if do_plot_transform_jacobian:
 
     for m, spi in enumerate(subplot_index):
         plt.subplot(n_rows, n_cols_total, spi)
-        plt.plot(wx_arc[:, :, m, 0, 0], wy_arc[:, :, m, 0, 0], '-', color='tab:blue', linewidth=1.5)
-        plt.plot(wx_grid[:, :, m, 0, 0], wy_grid[:, :, m, 0, 0], '-k', linewidth=0.7)
-        plt.plot(wx_grid[:, :, m, 0, 0].T, wy_grid[:, :, m, 0, 0].T, '-k', linewidth=0.7)
+        plt.plot(wx_arc[:, :, m, 0, 0], wy_arc[:, :, m, 0, 0], '-', color='tab:blue', linewidth=arc_linewidth)
+        plt.plot(wx_grid[:, :, m, 0, 0], wy_grid[:, :, m, 0, 0], '-k', linewidth=grid_linewidth)
+        plt.plot(wx_grid[:, :, m, 0, 0].T, wy_grid[:, :, m, 0, 0].T, '-k', linewidth=grid_linewidth)
         plt.xlim((domain['x_min'], domain['x_max']))
         plt.ylim((domain['y_min'], domain['y_max']))
         plt.xticks([])
         plt.yticks([])
 
+    # Plot amplitude
+    middletop_spi = n_cols_basis + 1
+    ax_middletop = plt.subplot(n_rows, n_cols_total, (middletop_spi, middletop_spi + 1))
+    ax_middletop.set_aspect(1)
+    x_grid_disk = x_grid.clone()
+    y_grid_disk = y_grid.clone()
+    x_grid_disk[r_mask] = np.nan
+    y_grid_disk[r_mask] = np.nan
+    plt.plot(x_arc[:, :, 0, 0, 0], y_arc[:, :, 0, 0, 0], '-', color='tab:blue', linewidth=arc_linewidth)
+    plt.plot(x_grid_disk[:, :, 0, 0, 0], y_grid_disk[:, :, 0, 0, 0], '-k', linewidth=grid_linewidth)
+    plt.plot(x_grid_disk[:, :, 0, 0, 0].T, y_grid_disk[:, :, 0, 0, 0].T, '-k', linewidth=grid_linewidth)
+    plt.title('b. Grids (x, y)')
+    plt.xlim((domain['x_min'], domain['x_max']))
+    plt.ylim((domain['y_min'], domain['y_max']))
+    plt.xticks([])
+    plt.yticks([])
 
     # === Plot approximate amplitude functions, obtained from Jacobians === #
+    # Normalization factor
+    area = (domain['x_max'] - domain['x_min']) * (domain['y_max'] - domain['y_min'])
+    num_samples = np.prod(domain['yxshape'])
+    norm_factor = num_samples / area
+
+    # Amplitude truncated Gaussian
+    x, y = get_coords(domain)
+    amp_unnorm_sq = trunc_gaussian(x, y, **amplitude_kwargs) ** 2       # Non-normalized amplitude squared
+    amp_sq = amp_unnorm_sq * norm_factor / amp_unnorm_sq.sum()          # Normalized amplitude
+
+    # Amplitude rectangle
+    wx, wy, jacobian = coord_transform(x=x, y=y, a=a, b=b, p_tuple=p_tuple, q_tuple=q_tuple, compute_jacobian=True)
+    amp_rect_sq_transformed = amplitude_rectangle(x=wx, y=wy, domain=domain) ** 2   # Transformed amplitude rectangle
+    amp_sq_approx = norm_factor * (amp_rect_sq_transformed * jacobian.abs())        # Approx. to original amplitude²
+
     # vmax = amp_sq.max()
     vmax = 1.2
     for m, spi in enumerate(subplot_index):
@@ -149,16 +172,16 @@ if do_plot_transform_jacobian:
     plt.subplot(n_rows, n_cols_total, (center_spi, center_spi+1))
     plt.imshow(amp_sq[:, :, 0, 0, 0], vmin=0, vmax=vmax, cmap=cmap, extent=(-1, 0, -1, 1))
     draw_circle('--w', 1, np.pi / 2, 3 * np.pi / 2)
-    plt.title('c. $A(x, y)$')
+    plt.title('d. $A(x, y)$')
     plt.xticks([])
     plt.yticks([])
 
-    ax_cb = plt.axes((0.490, 0.798, 0.012, 0.16))
+    ax_cb = plt.axes((0.480, 0.05, 0.01, 0.28))
     plt.colorbar(cax=ax_cb)
     plt.yticks((0, 0.5, 1.0, vmax), ('0.0', '0.5', '1.0', f'$\\geq${vmax:.1f}'))
 
-    fig.text(0.23, 0.985, "a. Transformed grids $x'(x,y),\\;y'(x,y)$", ha='center', va='center', fontsize=14)
-    fig.text(0.77, 0.985, "b. $A_R(x', y')\\cdot |\\,J\,|$", ha='center', va='center', fontsize=14)
+    fig.text(0.23, 0.985, "a. Transformed grids (x', y')", ha='center', va='center', fontsize=14)
+    fig.text(0.77, 0.985, "c. $A_R(x', y')\\cdot |\\,J\,|$", ha='center', va='center', fontsize=14)
 
 
 # Plot a transformed bitmap image of a grid
