9f0e5703918b26e4148e4e4e88f2cb8aead16e9d
Daniel Cox <danielcox16@hotmail.com>
2024-09-06 18:28:00 +0200

    Plot line through bead and plot SLM field

--- experiment/analyze_compare_wfs.py
+++ experiment/analyze_compare_wfs.py
@@ -14,15 +14,28 @@ from phase_only_orthonormalization.mode_functions import trunc_gaussian
 localdata = '/home/dani/LocalData/wfs-OrthoFBDR-comparison/'
 # localdata = 'C:/LocalData/wfs-wfsr-comparison/'
 path_glob = 'set?/wfs-comparison_t*.npz'
-file_numbers_to_plot = list(range(0, 5))        # Which images to plot
+# file_numbers_to_include = list(range(0, 15))    # Which files to read and include in graph (at least two)
+file_numbers_to_include = [1, 12]
+file_numbers_to_plot = list(range(0, 5))        # From selection, which images to plot (non-existing are ignored)
 
 do_plot_parking_convergence = False
 
 # Image settings
-cmap = 'magma'
-slice0 = slice(0, 1000)
-slice1 = slice(0, 1000)
-circ_style = '--w'
+cmap = 'magma'                                      # Colormap
+slice0 = slice(0, 600)                              # Crop x
+slice1 = slice(0, 1000)                             # Crop y
+circ_style = '--w'                                  # Pupil circle
+
+flat_beadline_x = (544, 613)                        # Line through bead x coords, flat wavefront
+flat_beadline_y = (257, 257)                        # Line through bead y coords, flat wavefront
+algs_beadline_x = ((534, 603), (542, 611))          # Line through bead x coords, shaped wavefront algorithms
+algs_beadline_y = ((245, 245), (255, 255))          # Line through bead y coords, shaped wavefront algorithms
+beadline_flat_kwargs = {'color': (0.0, 0.9, 1.0), 'linewidth': 0.5}
+beadline_alg_kwargs = ({'color': (1.0, 0.7, 0.0), 'linewidth': 0.5}, {'color': (0.1, 1.0, 0.1), 'linewidth': 0.5})
+
+bead_flat_kwargs = {'color': 'tab:blue', 'linestyle': 'dashdot'}
+bead_alg0_kwargs = {'color': 'tab:orange', 'linestyle': 'dashed'}
+bead_alg1_kwargs = {'color': 'tab:green', 'linestyle': 'solid'}
 
 # Scalebar
 scalebar_props = {
@@ -38,16 +51,20 @@ ncols = 3
 
 # Title strings
 image_letters = ('a. ', 'b. ', 'c. ')
-pattern_letters = ('d. ', 'e. ', 'f. ')
+pattern_letters = ('e. ', 'f. ', 'g. ')
+beadline_letter = 'd.'
 basis_names = ('No correction', 'Plane wave basis', 'Our orthonormal basis')
 
 plt.rcParams['font.size'] = 13
 
+assert len(file_numbers_to_include) > 1
 
 # Use os.path.join to ensure the path is constructed correctly
-npz_files = sorted(glob.glob(os.path.join(localdata, path_glob)))
+npz_files_all = sorted(glob.glob(os.path.join(localdata, path_glob)))
+npz_files_sel = [npz_files_all[i] for i in file_numbers_to_include]
 
-print(f'Found {len(npz_files)} files.')
+print(f'Found {len(npz_files_all)} files.')
+print(f'Selected {len(file_numbers_to_include)} files.')
 
 # Initialize
 signal_enhancement = [[], []]
@@ -63,7 +80,7 @@ domain = {
     'x_max': 1,
     'y_min': -1,
     'y_max': 1,
-    'yxshape': (300, 300),           # Number of samples in each spatial direction
+    'yxshape': (1000, 1000),           # Number of samples in each spatial direction
 }
 x = np.linspace(domain['x_min'], domain['x_max'], domain['yxshape'][1]).reshape(1, -1)  # x coords
 y = np.linspace(domain['y_min'], domain['y_max'], domain['yxshape'][0]).reshape(-1, 1)  # y coords
@@ -93,7 +110,6 @@ def draw_circle(circ_style, r_circ):
 
 
 def add_scalebar(ax, width_m, height_m, pix_size_m, pad):
-
     scalebar_width_pix = width_m / pix_size_m
     scalebar_height_pix = height_m / pix_size_m
     scalebar = AnchoredSizeBar(ax.transData,
@@ -106,7 +122,17 @@ def add_scalebar(ax, width_m, height_m, pix_size_m, pad):
     ax.add_artist(scalebar)
 
 
-for n_f, filepath in enumerate(tqdm(npz_files)):
+def add_beadline(ax, beadline_x, beadline_y, beadline_kwargs):
+    ax.plot(beadline_x, beadline_y, **beadline_kwargs)
+
+
+def get_data_beadline(img, beadline_x, beadline_y, pix_avg_plusmin=3):
+    slice_y = slice(beadline_y[0]-pix_avg_plusmin, beadline_y[1]+pix_avg_plusmin)
+    slice_x = slice(beadline_x[0], beadline_x[1]+1)
+    return img[slice_y, slice_x].mean(axis=0)
+
+
+for n_f, filepath in enumerate(tqdm(npz_files_sel)):
 
     full_pattern_feedback_all.append([])
 
@@ -139,12 +165,14 @@ for n_f, filepath in enumerate(tqdm(npz_files)):
         plt.title(f'{image_letters[0]}{basis_names[0]}')
         plt.xticks([])
         plt.yticks([])
+        add_beadline(plt.gca(), flat_beadline_x, flat_beadline_y, beadline_flat_kwargs)
         add_scalebar(ax=plt.gca(), **scalebar_props)
 
         # Show phase patterns
         scale = 1 / np.abs(amplitude).max()
         phase_rgb = complex_to_rgb(amplitude, scale)
         plt.subplot(nrows, ncols, ncols+1)
+        plt.gca().set_position((0.172, 0.01, 0.141, 0.446))
         plt.imshow(phase_rgb, extent=(-1, 1, -1, 1))
         plt.title(f'{pattern_letters[0]}')
         plt.xticks([])
@@ -165,13 +193,19 @@ for n_f, filepath in enumerate(tqdm(npz_files)):
             plt.title(f'{image_letters[1+n_alg]}{basis_names[1+n_alg]}')
             plt.xticks([])
             plt.yticks([])
+            add_beadline(plt.gca(), algs_beadline_x[n_alg], algs_beadline_y[n_alg], beadline_alg_kwargs[n_alg])
             add_scalebar(ax=plt.gca(), **scalebar_props)
 
             # Show phase patterns
             tc = npz_data['wfs_results_all'][0][n_alg].t.conj()
-            scale = 1 / np.abs(tc).max()
-            phase_rgb = complex_to_rgb(tc, scale)
+            scale_tc = 1 / np.abs(tc).max()
+            field_at_slm = amplitude * np.exp(1j * np.angle(tc))
+            phase_rgb = complex_to_rgb(field_at_slm, 1)
             plt.subplot(nrows, ncols, 2+ncols+n_alg)
+            if n_alg == 0:
+                plt.gca().set_position((0.407, 0.01, 0.141, 0.446))
+            if n_alg == 1:
+                plt.gca().set_position((0.627, 0.01, 0.141, 0.446))
             plt.imshow(phase_rgb, extent=(-1, 1, -1, 1))
             plt.title(f'{pattern_letters[1 + n_alg]}')
             plt.xticks([])
@@ -189,13 +223,31 @@ for n_f, filepath in enumerate(tqdm(npz_files)):
 
     if n_f in file_numbers_to_plot:
         # Colorbar
-        ax_cb = plt.axes((0.937, 0.506, 0.014, 0.451))
+        ax_cb = plt.axes((0.937, 0.516, 0.014, 0.433))
         fig.colorbar(im0, cax=ax_cb)
         ax_cb.set_ylabel('Signal')
 
         # Colorwheel
-        ax_cw = plt.axes((0.91, 0.01, 0.08, 0.45))
+        ax_cw = plt.axes((0.84, 0.01, 0.08, 0.45))
         complex_colorwheel(ax=ax_cw, shape=(160, 160))
+
+        # Signal intensity line through bead
+        plt.axes((0.060, 0.120, 0.080, 0.282))
+
+        img_shaped_wf0 = npz_data['contrast_results_all'][0][0]['img_shaped_wf']
+        img_shaped_wf1 = npz_data['contrast_results_all'][0][1]['img_shaped_wf']
+        beadsignal_flat = get_data_beadline(img_flat_wf, flat_beadline_x, flat_beadline_y).squeeze()
+        beadsignal_alg0 = get_data_beadline(img_shaped_wf0, algs_beadline_x[0], algs_beadline_y[0]).squeeze()
+        beadsignal_alg1 = get_data_beadline(img_shaped_wf1, algs_beadline_x[1], algs_beadline_y[1]).squeeze()
+        num_pix_beadline = (flat_beadline_x[1] - flat_beadline_x[0] + 1)
+        xrange = np.linspace(0, scalebar_props['pix_size_m'] * 1e6 * num_pix_beadline, num_pix_beadline)
+
+        plt.plot(xrange, beadsignal_flat, label=basis_names[0], **bead_flat_kwargs)
+        plt.plot(xrange, beadsignal_alg0, label=basis_names[1], **bead_alg0_kwargs)
+        plt.plot(xrange, beadsignal_alg1, label=basis_names[2], **bead_alg1_kwargs)
+        plt.xlabel('position ($\\mu m$)')
+        plt.ylabel('Signal through bead')
+        plt.title(beadline_letter)
 
         # Plot parking convergence
         if do_plot_parking_convergence:
