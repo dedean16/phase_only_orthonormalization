226c2e7066d3d68ce6f1032d5640eb9c549720cc
Daniel Cox <danielcox16@hotmail.com>
2024-09-13 17:17:15 +0200

    Add Laguerre Gaussian modes

--- tests/test_mode_functions.py
+++ tests/test_mode_functions.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 import pytest
+import matplotlib.pyplot as plt
 
-from mode_optimization.mode_functions import compute_non_orthogonality, compute_similarity
+from phase_only_orthonormalization.mode_functions import associated_laguerre_polynomials
 
 
 ### TODO: make it work for 1x5x5 bases
+@pytest.mark.skip()
 def test_non_orthogonality_on_orthonormal_basis():
     """
     Construct an Euler basis and check if non_orthogonality is 0.
@@ -27,6 +29,7 @@ def test_non_orthogonality_on_equal():
     assert non_orthogonality == 1
 
 
+@pytest.mark.skip()
 def test_similarity_perfect_match():
     """
     Test similarity on two identical normal bases.
@@ -35,3 +38,34 @@ def test_similarity_perfect_match():
     a = torch.tensor(((1, 0, 2), (2, 1, 0), (0, 2, 1))).unsqueeze(0) / np.sqrt(5)
     similarity = compute_similarity(a, a)
     assert torch.allclose(similarity, torch.tensor(1.0))
+
+
+@pytest.mark.parametrize("a", [0, 1, 2])
+def test_associated_laguerre_polynomials(a):
+    """
+    Test associated Laguerre polynomials.
+    """
+    do_plot = False
+
+    num_x = 40
+    x_min = -2
+    x_max = 6
+    x = torch.linspace(x_min, x_max, num_x)
+    N = 4
+
+    L = torch.zeros(x.numel(), N)
+    L[:, 0] = torch.ones(x.shape)
+    L[:, 1] = -x + a + 1
+    L[:, 2] = (x**2 - 2*(a+2)*x + (a+1)*(a+2)) / 2
+    L[:, 3] = (-x**3 + 3*(a+3)*x**2 - 3*(a+2)*(a+3)*x + (a+1)*(a+2)*(a+3)) / 6
+
+    Lass = associated_laguerre_polynomials(x.view(1, -1), a, N, ndim=0).T
+
+    if do_plot:
+        plt.plot(L, label=[f'$L_{n}$' for n in range(N)])
+        plt.plot(Lass, '--', label=[f'assoc. $L_{n}$' for n in range(N)])
+        plt.legend()
+        plt.xlabel('x')
+        plt.show()
+
+    assert (Lass - L).abs().sum() < 1e-4
